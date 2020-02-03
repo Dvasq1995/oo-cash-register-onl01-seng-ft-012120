3b2a97b0307dbd6b95334adbcfcab4e931970fdf
Diego Vasquez <botellongo69@outlook.com>
2020-02-03 14:55:05 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
-
+class CashRegister
+  attr_accessor :total, :employee_discount
+  @items = []
+  
+  def initialize(employee_discount = 0)
+    @total = 0
+    @employee_discount = employee_discount
+  end
+  
+  def add_item(title, price, quantity = 0)
+    
+  end
+  
+  def apply_discount
+    
+  end
+  
+  def items
+    
+  end
+  
+  def void_last_transaction
+    
+  end
+end
